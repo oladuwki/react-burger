1fe3dac15a04ddf4b6fd6e4b3435edc242fcae4c
Nikita Dannik <nikitka.dannik@gmail.com>
2021-12-12 00:05:02 +0300

    sprint-4/step-1 "changing the project for typeScript"

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import App from './components/app/App';
-import { store } from './services/store.jsx';
+import App from './components/app/app';
+import { BrowserRouter } from "react-router-dom";
+import { createStore, compose, applyMiddleware } from 'redux';
+import { rootReducer } from './services/reducers';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+const composeEnhancers =
+  typeof window === 'object' && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(thunk));
+
+const store = createStore(rootReducer, enhancer);
 
 ReactDOM.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <Provider store={store}>
-                <App />
-            </Provider>
-        </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <BrowserRouter basename="/">
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
