d03956117a6c11b2e89a86949212d76472885183
Nikita Dannik <nikitka.dannik@gmail.com>
2022-01-10 23:31:50 +0300

    sprint5/step1

--- src/services/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import { burgerVendorReducer } from './burgerVendor';
-import { userReducer } from './user'
-
-export const rootReducer = combineReducers({
-    burgerVendor: burgerVendorReducer,
-    user: userReducer,
-  });
